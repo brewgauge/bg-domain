4091d694968ae6a81e8f875a5e2741ca25569a72
Marco Piraccini <marco.piraccini@gmail.com>
2016-06-22 14:17:57 +0200

    Removed useless callback. Now an exception is thrown if checksum fails

--- test/adcToCelsius.js
+++ test/adcToCelsius.js
@@ -4,19 +4,20 @@ var test = require('tape')
 var adcToCelsius = require('../lib/adcToCelsius')
 
 test('ADC to Celsius - Wrong checksum', function (t) {
-  adcToCelsius(2075, 20, (err, temp) => {
-    if (!err) {
-      t.fail('Wrong checksum ,an error should be generated')
-    }
+  try {
+    var temp = adcToCelsius(2075, 20)
+    t.fail(`Wrong checksum ,an error should be generated, instead returned ${temp}`)
+  } catch (error) {
     t.end()
-  })
+  }
 })
 
 test('ADC to Celsius - Correct conversion', function (t) {
-  adcToCelsius(2075, 3406, (err, temp) => {
-    t.error(err)
-    console.log(typeof temp)
-    t.equal(temp, 70.830, 'Check that the 2075 ADC => 47.939 C')
+  try {
+    var temp = adcToCelsius(2075, 3406)
+    t.equal(temp, 70.830, 'Check that the 2075 ADC => 70.830 C') // TODO: Verify if this conversion is correct.
     t.end()
-  })
+  } catch (error) {
+    t.fail(error)
+  }
 })
